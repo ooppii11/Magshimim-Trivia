f0013afbd93d3647ff38392eb3d3d0852e934a60
Aviv Hazan <aviv6715@gmail.com>
2023-04-26 14:40:42 +0300

    Update tester and add hello test

--- Tests/tester.py
+++ Tests/tester.py
@@ -3,11 +3,23 @@ import socket
 
 ADRESS = "127.0.0.1"
 PORT = 5555
+TESTS = [
+    project_tests.hello_test
+]
+
+
+def execute_tests(socket):
+    for test in TESTS:
+        if output:=type(test(socket)) == tuple:
+            print(output[1])
+            return
+    print("The program pass the tests:({}/{})".format(len(TESTS), len(TESTS)))
+
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((ADRESS, PORT))
-        project_tests.hello_test.hello_test(sock)
+        execute_tests(sock)
 
 
 if __name__ == "__main__":
