bc01175d90b4f0accafc2b6596d76217206c2ac5
Aviv Hazan <aviv6715@gmail.com>
2023-05-05 15:42:28 +0300

    Add Room class

---
+++ Trivia/Room.cpp
@@ -0,0 +1,29 @@
+#include "Room.h"
+#include <algorithm>
+
+
+void Room::addUser(LoggedUser user)
+{
+	this->_users.push_back(user);
+}
+
+void Room::removeUser(LoggedUser user)
+{
+	auto userIt = std::find(this->_users.begin(), this->_users.end(), user);
+	if (userIt == this->_users.end())
+	{
+		throw std::exception("User isn't in the room");
+	}
+	this->_users.erase(userIt);
+}
+
+std::vector<std::string> Room::getAllUsers()
+{
+	std::vector<std::string> usersNames;
+
+	for (auto user : this->_users)
+	{
+		usersNames.push_back(user.getUsername());
+	}
+	return usersNames;
+}
--- Trivia/Room.h
+++ Trivia/Room.h
@@ -1,1 +1,26 @@
 #pragma once
+#include <vector>
+#include "LoggedUser.h"
+
+
+typedef struct RoomData
+{
+	unsigned int id;
+	std::string name;
+	unsigned int maxPlayers;
+	unsigned int numOfQuestionsInGame;
+	unsigned int timePerQuestion;
+	unsigned int isActive;
+} RoomData;
+
+class Room
+{
+public:
+	void addUser(LoggedUser user);
+	void removeUser(LoggedUser user);
+	std::vector<std::string> getAllUsers();
+
+private:
+	RoomData _metadata;
+	std::vector<LoggedUser> _users;
+};
