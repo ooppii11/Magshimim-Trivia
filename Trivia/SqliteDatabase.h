d8f8c6f8430d2e90068e561904b3c157f8f80dc0
Aviv Hazan <aviv6715@gmail.com>
2023-05-05 19:04:20 +0300

    Update sqlitedb.h

--- Trivia/SqliteDatabase.h
+++ Trivia/SqliteDatabase.h
@@ -19,6 +19,30 @@ public:
 	bool doesPasswordMatch(std::string username, std::string password) const override;
 	void addNewUser(std::string username, std::string password, std::string email) override;
 
+	// Categories:
+	virtual void addNewCategory(std::string CategoryName, bool permission, int creatorId);
+	virtual bool doesCategoryExist(std::string CategoryName) const;
+
+	// Questions:
+	virtual void addNewQuestion(std::string CategoryName, int userId, Question question);
+	virtual void removeQuestion(int questionId, int userId) = 0;
+	virtual std::vector<Question> getCategoryQuestions(std::string CategoryName, int numberOfQuestions) const;
+
+	// History:
+	virtual void addNewHistory(History history);
+	virtual std::vector<History> getUserLastFiveHistory(int usrId) const;
+	virtual std::vector<History> getCategoryHistory(int categoryId) const;
+
+	// Statistics:
+	virtual void updatUserNumberofAnswer(int userId, bool correctAnswer, double time);
+	virtual void updatUserNumberofgames(int userId);
+
+	virtual float getPlayerAverageAnswerTime(std::string username) const;
+	virtual int getNumOfCorrectAnswers(std::string username) const;
+	virtual int getNumOfTotalAnswers(std::string username) const;
+	virtual int getNumOfPlayerGames(std::string username) const;
+	virtual int getPlayerScore(std::string username) const;
+	virtual std::vector<float> getHighScores(int numberOfUsers) const;
 
 private:
 	sqlite3* _db;
