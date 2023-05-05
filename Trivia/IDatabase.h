2a1a874d6648daff88298447658866fb60f660fd
Aviv Hazan <aviv6715@gmail.com>
2023-05-05 19:00:37 +0300

    Update db interface

--- Trivia/IDatabase.h
+++ Trivia/IDatabase.h
@@ -1,11 +1,62 @@
 #pragma once
 #include <string>
+#include <vector>
+#include <ctime>
+
+
+typedef struct Question
+{
+	int correctAnswerIndex;
+	std::string firstAnswer;
+	std::string secondAnswer;
+	std::string thirdAnswer;
+	std::string fourthAnswer;
+} Question;
+
+
+typedef struct History
+{
+	int userId;
+	int categoryId;
+	int rank;
+	double avergeTime;
+	int answers;
+	int correctAnswers;
+	time_t creationDate;
+} History;
 
 
 class IDatabase 
 {
 public:
+	// Users:
 	virtual bool doesUserExist(std::string username) const = 0;
 	virtual bool doesPasswordMatch(std::string username, std::string password) const = 0;
 	virtual void addNewUser(std::string username, std::string password, std::string email) = 0;
+
+	// Categories:
+	virtual void addNewCategory(std::string CategoryName, bool permission, int creatorId) = 0;
+	virtual bool doesCategoryExist(std::string CategoryName) const = 0;
+
+	// Questions:
+	virtual void addNewQuestion(std::string CategoryName, int userId, Question question) = 0;
+	virtual void removeQuestion(int questionId, int userId) = 0;
+	virtual std::vector<Question> getCategoryQuestions(std::string CategoryName, int numberOfQuestions) const = 0;
+
+	// History:
+	virtual void addNewHistory(History history) = 0;
+	virtual std::vector<History> getUserLastFiveHistory(int usrId) const = 0;
+	virtual std::vector<History> getCategoryHistory(int categoryId) const = 0;
+
+	// Statistics:
+	virtual void updatUserNumberofAnswer(int userId, bool correctAnswer, double time) = 0;
+	virtual void updatUserNumberofgames(int userId) = 0;
+
+	virtual float getPlayerAverageAnswerTime(std::string username) const = 0;
+	virtual int getNumOfCorrectAnswers(std::string username) const = 0;
+	virtual int getNumOfTotalAnswers(std::string username) const = 0;
+	virtual int getNumOfPlayerGames(std::string username) const = 0;
+	virtual int getPlayerScore(std::string username) const = 0;
+	virtual std::vector<float> getHighScores(int numberOfUsers) const = 0;
+
 };
