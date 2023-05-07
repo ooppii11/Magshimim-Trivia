299aa3b5667a43061f2d8a072d28af1b15535744
Itamar Levy <itamar.levyhe@gmail.com>
2023-05-07 19:19:02 +0300

    Deserializer functions signature

--- Trivia/JsonRequestPacketDeserializer.h
+++ Trivia/JsonRequestPacketDeserializer.h
@@ -7,5 +7,12 @@ class Deserializer
 public:
 	static LoginRequest deserializeLoginRequest(const Buffer& buffer);
 	static SignupRequest deserializeSignupRequest(const Buffer& buffer);
-	
+	static GetPlayersInRoomRequest deserializeGetPlayersRequest(const Buffer& buffer);
+	static JoinRoomRequest deserializeJoinRoomRequest(const Buffer& buffer);
+	static CreateRoomRequest deserializeCreateRoomRequest(const Buffer& buffer);
+
+	static addCategorie addCategorieToUser(const Buffer& buffer);
+	static removeCategorie removeCategorieFromUser(const Buffer& buffer);
+	static addQuestion addQuestionToUserCategorie(const Buffer& buffer);
+	static removeQuestion removeQuestionFromUserCategorie(const Buffer& buffer);
 };
