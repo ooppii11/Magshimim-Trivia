ac487845369640e59da394ff988652160189c562
Itamar Levy <itamar.levyhe@gmail.com>
2023-05-14 18:49:27 +0300

    update CategorieManager to QuizManager

--- Trivia/QuizManager.h
+++ Trivia/QuizManager.h
@@ -3,14 +3,16 @@
 #include <memory>
 #include <string>
 #include "IDatabase.h"
-typedef unsigned int roomID;
 
-class CategorieManager
+class QuizManager
 {
 public:
-	CategorieManager(std::shared_ptr<IDatabase> db);
+	QuizManager(std::shared_ptr<IDatabase> db);
 	std::vector<std::pair<std::string, int>> getPublicCategories() const;
 	std::vector<std::pair<std::string, int>> getPrivagteCategories(const std::string& username);
+	void addNewCategory(Category category, const std::string& username);
+	void addNewQuestion(const unsigned int categorieId, std::string  username, Question question);
+	std::vector<Question> getCategoryQuestions(const unsigned int categorieId) const;
 
 private:
 	std::shared_ptr<IDatabase> _database;
