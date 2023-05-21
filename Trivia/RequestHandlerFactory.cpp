93e84d0803f48053958a62088009417702a381a5
Aviv Hazan <aviv6715@gmail.com>
2023-05-21 06:05:39 +0300

    Add delete methods

--- Trivia/RequestHandlerFactory.cpp
+++ Trivia/RequestHandlerFactory.cpp
@@ -1,11 +1,21 @@
 #include "RequestHandlerFactory.h"
 
 RequestHandlerFactory::RequestHandlerFactory(std::shared_ptr<IDatabase> datbase) :
-	_database(datbase), _loginManager(_database), _roomManager(), _statisticsManager(_database) {}
+	_database(datbase), _loginManager(_database), _roomManager(), _statisticsManager(_database), _quizManager(_database), _historyManager(_database) {}
 
 std::shared_ptr<LoginRequestHandler> RequestHandlerFactory::createLoginRequestHandler()
 {
 	return std::shared_ptr<LoginRequestHandler>(new LoginRequestHandler(this->_loginManager, *this));
+}
+
+StatisticsManager& RequestHandlerFactory::getStatisticsManager()
+{
+	return this->_statisticsManager;
+}
+
+RoomManager& RequestHandlerFactory::getRoomManager()
+{
+	return this->_roomManager;
 }
 
 LoginManager& RequestHandlerFactory::getLoginManager()
@@ -15,5 +25,5 @@ LoginManager& RequestHandlerFactory::getLoginManager()
 
 std::shared_ptr<MenuRequestHandler> RequestHandlerFactory::createMenuRequestHandler(LoggedUser user)
 {
-	return std::shared_ptr<MenuRequestHandler>(new MenuRequestHandler(user, this->_roomManager, this->_statisticsManager, *this));
+	return std::shared_ptr<MenuRequestHandler>(new MenuRequestHandler(user, this->_roomManager, this->_statisticsManager, *this, this->_quizManager));
 }
