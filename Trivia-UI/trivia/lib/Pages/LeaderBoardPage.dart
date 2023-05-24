b7c04dcbbc2b4af2885378c890ba03bda53ff4d0
Aviv Hazan <aviv6715@gmail.com>
2023-05-24 16:16:41 +0300

    Add user

--- Trivia-UI/trivia/lib/Pages/LeaderBoardPage.dart
+++ Trivia-UI/trivia/lib/Pages/LeaderBoardPage.dart
@@ -17,66 +17,85 @@ class _LeaderBoardPage extends State<LeaderBoardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.person, color: Colors.grey[600]), label: ''),
-            BottomNavigationBarItem(icon: Icon(Icons.add_box), label: ''),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.leaderboard_rounded,
-                  color: Colors.blue,
-                ),
-                label: ''),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.home,
-                ),
-                label: ''),
-          ],
-          onTap: (value) {
-            if (value == 0)
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => UserPage(
-                    socketService: widget.socketService,
-                  ),
-                ),
-              );
-            if (value == 3)
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => HomePage(
-                    socketService: widget.socketService,
-                  ),
-                ),
-              );
-          }),
-      appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          actions: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(right: 20.0),
-              child: IconButton(
+        bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person, color: Colors.grey[600]), label: ''),
+              BottomNavigationBarItem(icon: Icon(Icons.add_box), label: ''),
+              BottomNavigationBarItem(
                   icon: Icon(
-                    Icons.logout,
-                    color: Colors.black,
-                    size: 26.0,
+                    Icons.leaderboard_rounded,
+                    color: Colors.blue,
                   ),
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (_) => LoginPage(
-                                  socketService: widget.socketService,
-                                )));
-                  }),
+                  label: ''),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home,
+                  ),
+                  label: ''),
+            ],
+            onTap: (value) {
+              if (value == 0)
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => UserPage(
+                      socketService: widget.socketService,
+                    ),
+                  ),
+                );
+              if (value == 3)
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => HomePage(
+                      socketService: widget.socketService,
+                    ),
+                  ),
+                );
+            }),
+        appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            actions: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 20.0),
+                child: IconButton(
+                    icon: Icon(
+                      Icons.logout,
+                      color: Colors.black,
+                      size: 26.0,
+                    ),
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) => LoginPage(
+                                    socketService: widget.socketService,
+                                  )));
+                    }),
+              ),
+            ]),
+        body: SingleChildScrollView(
+          child: Column(children: [
+            Text("Leadrboard"),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  gradient: LinearGradient(colors: [
+                    Colors.yellow.shade600,
+                    Colors.orange,
+                    Colors.red
+                  ])),
+              child: Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: Container(
+                  height: 600,
+                ),
+              ),
             ),
           ]),
-    );
+        ));
   }
 }
---
+++ Trivia-UI/trivia/lib/user.dart
@@ -0,0 +1,14 @@
+class User {
+  String _username;
+  int _score;
+
+  User(this._username, this._score);
+
+  String getUsername() {
+    return _username;
+  }
+
+  int getScore() {
+    return _score;
+  }
+}
