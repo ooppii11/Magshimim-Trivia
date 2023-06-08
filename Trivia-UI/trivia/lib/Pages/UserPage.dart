24a5975b57e1a6dc92600ff63bdcf5259e255619
Itamar Levy <itamar.levyhe@gmail.com>
2023-06-08 14:28:33 +0300

    flutter build - design

--- Trivia-UI/trivia/lib/Pages/UserPage.dart
+++ Trivia-UI/trivia/lib/Pages/UserPage.dart
@@ -3,6 +3,7 @@ import 'package:trivia/Pages/leaderBoardPage.dart';
 import 'package:trivia/SocketService.dart';
 import 'package:flutter/material.dart';
 import 'package:trivia/Pages/loginPage.dart';
+import 'package:trivia/message.dart';
 
 class UserPage extends StatefulWidget {
   final SocketService socketService;
@@ -14,10 +15,31 @@ class UserPage extends StatefulWidget {
 }
 
 class _UserPage extends State<UserPage> {
+  late List<Map<String, dynamic>> _history;
+  late Map<String, dynamic> _statistics;
+
+  _UserPage()
+  {
+    this._history = [];
+    this._statistics = {}; 
+    getHistory();
+    getStatistics();
+  }
+
+  void getHistory() async {
+    
+  }
+  
+  void getStatistics() async {
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
           backgroundColor: Colors.transparent,
           elevation: 0,
           actions: <Widget>[
@@ -39,11 +61,128 @@ class _UserPage extends State<UserPage> {
                   }),
             ),
           ]),
-      // User page content:
-      // TODO: add a card that is on the top center and has user private stats
-      // TODO: add a container that has user last five games
-      
-      backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  child: Column(
+                    children: [
+                      Text(
+                        'STATS',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 16),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 5,
+                          ),
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.blue.shade200,
+                              Colors.green.shade50,
+                              Colors.cyan.shade300
+                            ],
+                          ),
+                        ),
+                        height: MediaQuery.of(context).size.height * 0.4,
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        child: SingleChildScrollView(
+                          child: DefaultTextStyle(
+                            style: const TextStyle(color: Colors.black),
+                            child: DataTable(
+                              dataTextStyle: const TextStyle(color: Colors.black),
+                              columns: const [
+                                DataColumn(
+                                  label: Text('Statistics Name'),
+                                ),
+                                DataColumn(
+                                  label: Text('User Results'),
+                                )
+                              ],
+                              rows: _statistics.entries.map((entry) {
+                                return DataRow(
+                                  cells: [
+                                    DataCell(Text(entry.key)),
+                                    DataCell(Text(entry.value.toString())),
+                                  ],
+                                );
+                              }).toList(),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 16),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    children: [
+                      Text(
+                        'HISTORY',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 16),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 5,
+                          ),
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.blue.shade200,
+                              Colors.green.shade50,
+                              Colors.cyan.shade300
+                            ],
+                          ),
+                        ),
+                        height: MediaQuery.of(context).size.height * 0.4,
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        child: SingleChildScrollView(
+                          child: DefaultTextStyle(
+                            style: const TextStyle(color: Colors.black),
+                            child: DataTable(
+                              dataTextStyle: const TextStyle(color: Colors.black),
+                              columns: const [
+                                DataColumn(
+                                  label: Text('Num'),
+                                ),
+                                DataColumn(
+                                  label: Text('Category'),
+                                ),
+                                DataColumn(
+                                  label: Text('Score'),
+                                ),
+                              ],
+                              rows: _history.map((entry) {
+                                return DataRow(
+                                  cells: [
+                                    DataCell(Text((_history.indexOf(entry) + 1).toString())),
+                                    DataCell(Text(entry['category'].toString())),
+                                    DataCell(Text(entry['score'].toString())),
+                                  ],
+                                );
+                              }).toList(),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+         backgroundColor: Colors.white,
       bottomNavigationBar: BottomNavigationBar(
           type: BottomNavigationBarType.fixed,
           items: [
@@ -80,6 +219,7 @@ class _UserPage extends State<UserPage> {
               );
             }
           }),
+      ),
     );
   }
 }
