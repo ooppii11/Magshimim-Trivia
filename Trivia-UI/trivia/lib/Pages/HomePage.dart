1647a6723ca1f21c1f197f7df010744d020f0b11
Aviv Hazan <aviv6715@gmail.com>
2023-06-09 11:20:34 +0300

    Fix conflicts

--- Trivia-UI/trivia/lib/Pages/HomePage.dart
+++ Trivia-UI/trivia/lib/Pages/HomePage.dart
@@ -12,7 +12,7 @@ import 'package:trivia/message.dart';
 // ignore_for_file: prefer_const_constructors
 
 // ignore: must_be_immutable
-int GET_CATEGORIES_CODE = 7;
+int GET_CATEGORIES_CODE = 6;
 int CREATE_ROOM_REQUEST_CODE = 11;
 int ERROR_CODE = 99;
 
@@ -54,15 +54,12 @@ class _HomePage extends State<HomePage> {
     _socketService.sendMessage(Message(GET_CATEGORIES_CODE, {}));
     final Message response = await _socketService.receiveMessage();
 
-    //Map<String, dynamic> data = response.getData();
-    Map<int, dynamic> data = response.getData();
-    /*
-    for (var categoryString in data) {
+    Map<String, dynamic> data = response.getData();
+    for (var categoryString in data["publicCategories"]) {
       this
           ._categories
-          .add(Category(categoryString[0], categoryString[1], true));
+          .add(Category(categoryString[1], categoryString[0], true));
     }
-    */
   }
 
   @override
@@ -117,17 +114,17 @@ class _HomePage extends State<HomePage> {
                     color: Colors.black,
                     size: 26.0,
                   ),
-                  onPressed: () async{
+                  onPressed: () async {
                     _socketService.sendMessage(Message(3, {}));
-                    final Message response = await _socketService.receiveMessage();
-                    if(response.getCode() == 2)
-                    {
+                    final Message response =
+                        await _socketService.receiveMessage();
+                    if (response.getCode() == 2) {
                       Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (_) => LoginPage(
-                                  socketService: widget.socketService,
-                                )));
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) => LoginPage(
+                                    socketService: widget.socketService,
+                                  )));
                     }
                   }),
             ),
@@ -223,13 +220,14 @@ class _HomePage extends State<HomePage> {
   }
 
   Future<bool> createRoom(Category category) async {
-    this._socketService.sendMessage(Message(CREATE_ROOM_REQUEST_CODE, {
-          "categorieId": category.getId(),
-          "maxPlayers": maxNumberOfPlayers,
-          "questionCount": numberOfQuestions,
-          "answerTimeout": maxTime,
-          "name": ""
-        }));
+    Map<String, dynamic> data = {
+      "categorieId": category.getId(),
+      "maxPlayers": int.parse(maxNumberOfPlayers.text),
+      "questionCount": int.parse(numberOfQuestions.text),
+      "answerTimeout": int.parse(maxTime.text),
+      "name": ""
+    };
+    this._socketService.sendMessage(Message(CREATE_ROOM_REQUEST_CODE, data));
     final Message response = await this._socketService.receiveMessage();
     return response.getCode() != ERROR_CODE;
   }
