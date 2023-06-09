4ff38ad1022ebb01f2e0d8879462a602881563d9
Aviv Hazan <aviv6715@gmail.com>
2023-06-09 15:17:40 +0300

    update factory handler

--- Trivia-UI/trivia/.dart_tool/dartpad/web_plugin_registrant.dart
+++ Trivia-UI/trivia/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -7,10 +7,12 @@
 // ignore_for_file: type=lint
 
 import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:toast/toast_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   FluttertoastWebPlugin.registerWith(registrar);
+  ToastWebPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
