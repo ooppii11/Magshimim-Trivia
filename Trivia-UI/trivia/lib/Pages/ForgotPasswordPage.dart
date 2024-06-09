597356098d87038e186d221d25382af71f4047bc
ooppii11 <itamar.levyhe@gmail.com>
2024-06-09 14:25:44 +0300

    modified gitignore

--- Trivia-UI/trivia/lib/Pages/ForgotPasswordPage.dart
+++
@@ -1,8 +0,0 @@
-import 'basePage.dart';
-import 'package:trivia/SocketService.dart';
-
-class ForgotPasswordPage extends BasePage {
-  const ForgotPasswordPage({super.key, 
-    required SocketService socketService,
-  }) : super(socketService: socketService);
-}
